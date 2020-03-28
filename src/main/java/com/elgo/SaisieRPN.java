92e7ce49df5f23bf1f63f815c40085adb7cc8880
misterelgo <misterelgo@gmail.com>
2020-03-28 22:17:00 +0000

    SaisieRPN added ...

--- src/main/java/com/elgo/SaisieRPN.java
+++ src/main/java/com/elgo/SaisieRPN.java
@@ -2,4 +2,30 @@ package com.elgo;
 import java.util.*;
 public class SaisieRPN {
     private static Scanner input = new Scanner(System.in);
+    private  MoteurRPN moteurRPN = new MoteurRPN();
+    private String inputValue =  new String();
+
+    public void acceuil (){
+        moteurRPN.moteurInit();
+
+        do {
+            inputValue = input.nextLine();
+            if (verifInput(inputValue)== false){
+                moteurRPN.apply(inputValue);
+            }else {
+                moteurRPN.enregistrer(Double.parseDouble(inputValue));
+            }
+            moteurRPN.affiche();
+        }while (true);
+
+    }
+
+    private boolean verifInput(String value){
+        try {
+            double check = Double.parseDouble(value);
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
 }
